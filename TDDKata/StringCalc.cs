d02e6b0fd0eae580a4ce8e74fb27ffea925884dd
Pavel Tsytovich <pavel.tsytovich@gmail.com>
2020-12-07 09:53:15 +0300

    Initial commit

---
+++ TDDKata/StringCalc.cs
@@ -0,0 +1,14 @@
+﻿// NUnit 3 tests
+// See documentation : https://github.com/nunit/docs/wiki/NUnit-Documentation
+using System;
+
+namespace TDDKata
+{
+    internal class StringCalc
+    {
+        internal int Add(string v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
