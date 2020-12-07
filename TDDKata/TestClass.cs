0c167b8d9384aa7bf58ff52f2d026e34ee9715c1
Zagidullin.RI <Rushan.developer@gmail.com>
2020-12-07 16:17:08 +0500

    Added tests

--- TDDKata/TestClass.cs
+++ TDDKata/TestClass.cs
@@ -16,5 +16,82 @@ namespace TDDKata
             int value = calc.Sum("2,2");
             Assert.That(value, Is.EqualTo(4), "Wrong actual value");
         }
+        
+        [Test]
+        public void TestSumOneNumber()
+        {
+            StringCalc calc = new StringCalc();
+            string argument = "2";
+            int expected = 2;
+            
+            int value = calc.Sum(argument);
+            Assert.That(value, Is.EqualTo(expected), "Wrong actual value");
+        }
+        
+        [Test]
+        public void TestSumWithZero()
+        {
+            StringCalc calc = new StringCalc();
+            string argument = "2,0";
+            int expected = 2;
+            
+            int value = calc.Sum(argument);
+            Assert.That(value, Is.EqualTo(expected), "Wrong actual value");
+        }
+        
+        [Test]
+        public void TestEmptyString()
+        {
+            StringCalc calc = new StringCalc();
+            string argument = "";
+            int expected = 0;
+            
+            int value = calc.Sum(argument);
+            Assert.That(value, Is.EqualTo(expected), "Wrong actual value");
+        }
+        
+        [Test]
+        public void TestNullString()
+        {
+            StringCalc calc = new StringCalc();
+            string argument = null;
+            int expected = -1;
+            
+            int value = calc.Sum(argument);
+            Assert.That(value, Is.EqualTo(expected), "Wrong actual value");
+        }
+        
+        [Test]
+        public void TestNegativeNumbers()
+        {
+            StringCalc calc = new StringCalc();
+            string argument = "4,-1";
+            int expected = -1;
+            
+            int value = calc.Sum(argument);
+            Assert.That(value, Is.EqualTo(expected), "Wrong actual value");
+        }
+        
+        [Test]
+        public void TestWrongNumbers()
+        {
+            StringCalc calc = new StringCalc();
+            string argument = "4,not a number";
+            int expected = -1;
+            
+            int value = calc.Sum(argument);
+            Assert.That(value, Is.EqualTo(expected), "Wrong actual value");
+        }
+        
+        [Test]
+        public void TestTooManyNumbers()
+        {
+            StringCalc calc = new StringCalc();
+            string argument = "1,2,3";
+            int expected = -1;
+            
+            int value = calc.Sum(argument);
+            Assert.That(value, Is.EqualTo(expected), "Wrong actual value");
+        }
     }
 }
